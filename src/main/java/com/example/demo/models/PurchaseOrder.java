550f13dadd82108b8708923618e55eab545fdbd0
danny.sun <danny.sun@grabtaxi.com>
2020-05-07 13:04:05 +0800

    Add entities

---
+++ src/main/java/com/example/demo/models/PurchaseOrder.java
@@ -0,0 +1,36 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+public class PurchaseOrder {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    List<PlantReservation> reservations;
+    PlantInventoryEntry plant;
+
+    @Embedded
+    BusinessPeriod businessPeriod;
+
+    @Column(precision = 8, scale = 2)
+    BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    POStatus status;
+    LocalDate startDate;
+    LocalDate endDate;
+}
