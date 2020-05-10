c177ba0c449a31a2cc42458b96f7aaad0811662f
danny.sun <danny.sun@grabtaxi.com>
2020-05-10 19:09:31 +0800

    Add Unit Tests

--- src/main/java/com/example/demo/models/PlantInventoryEntryRepository.java
+++ src/main/java/com/example/demo/models/PlantInventoryEntryRepository.java
@@ -3,7 +3,12 @@ package com.example.demo.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long> {
+
+    List<PlantInventoryEntry> findByNameContaining(String str);
+
 }
--- src/test/java/com/example/demo/models/InventoryRepositoryTest.java
+++ src/test/java/com/example/demo/models/InventoryRepositoryTest.java
@@ -1,16 +1,36 @@
 package com.example.demo.models;
 
 import com.example.demo.DemoApplication;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
 @Sql(scripts= "data.sql")
 @DirtiesContext(classMode=DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class InventoryRepositoryTest {
+    @Autowired
+    PlantInventoryEntryRepository plantInventoryEntryRepository;
+    @Autowired
+    PlantInventoryItemRepository plantInventoryItemRepository;
+    @Autowired
+    PlantReservationRepository plantReservationRepository;
+
+    @Test
+    public void queryPlantCatalog() {
+        assertThat(plantInventoryEntryRepository.count()).isEqualTo(14L);
+    }
+
+    @Test
+    public void queryByName() {
+        assertThat(plantInventoryEntryRepository.findByNameContaining("Mini").size()).isEqualTo(2);
+    }
 
 }
